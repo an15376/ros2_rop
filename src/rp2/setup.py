9a308da0093da2cdcef19b827188cddba6a01d71
kiki <61826510+an15376@users.noreply.github.com>
2024-05-08 01:03:34 +0900

    Update setup.py

--- src/rp2/setup.py
+++ src/rp2/setup.py
@@ -2,7 +2,7 @@ import os
 from glob import glob
 from setuptools import find_packages, setup
 
-package_name = 'rp2'
+package_name = 'rp2' # 패키지 이름 설정
 
 setup(
     name=package_name,
@@ -14,16 +14,16 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name), glob('launch/*_launch.py')),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools'], # 필요한 설치 의존성
     zip_safe=True,
     maintainer='kiki',
     maintainer_email='kiki@todo.todo',
     description='TODO: Package description',
     license='Apache-2.0',
-    tests_require=['pytest'],
+    tests_require=['pytest'], # 테스트에 필요한 의존성
     entry_points={
-        'console_scripts': [
-            'node1 = rp2.node1:main',
+        'console_scripts': [ 
+            'node1 = rp2.node1:main', # 실행 가능한 명령어와 해당 함수 매핑
             'node2 = rp2.node2:main',
         ],
     },
